5dde58a2f29b0be0bd60a60bb6c472266f499cb9
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-04-10 17:13:31 +0300

    Rename C API to *-bsp-ffi

---
+++ examples/headsail-bsp-ffi/src/lib.rs
@@ -0,0 +1,20 @@
+//! C API for `headsail-bsp`
+#![no_std]
+#![no_main]
+
+// Make symbols available in the C header by making a symbol visible here, and according to the
+// following format:
+//
+// - Function: `#[no_mangle] pub extern fn` ("functions")
+// - Global: `#[no_mangle] pub static`
+// - Constant: `pub const`
+//
+// To make types usable in C, make them `#[repr(C)]`, `#[repr(u8, u16, ... etc)]`, or
+// `#[repr(transparent)]`.
+//
+// Find more documentation at <https://github.com/mozilla/cbindgen/blob/master/docs.md>
+
+#[no_mangle]
+pub extern "C" fn putc(byte: u8) {
+    headsail_bsp::uart::putc(byte)
+}
