0747c7fb9a686841d6a8c315c0278c84ea07886c
Vaino Granat <vaino-waltteri.granat@tuni.fi>
2024-04-30 15:06:21 +0300

    Adds alloc feature for hpc (#15)
    Alloc enables the used of heap in bsp. Alloc requires more
    memory than bootram has so sdram needs to be enabled to use this
    feature.
    * Adds alloc feature hpc
    * Adds example for alloc feature
    * Adds sdram feature to bsp

--- examples/headsail-bsp/build.rs
+++ examples/headsail-bsp/build.rs
@@ -7,6 +7,14 @@ fn main() {
     // Put link script in our output directory and ensure it's on the linker search path
     let out = &path::PathBuf::from(env::var_os("OUT_DIR").unwrap());
     fs::copy("mem_hpc.x", out.join("mem_hpc.x")).unwrap();
+    if cfg!(feature = "hpc") {
+        if cfg!(feature = "sdram") {
+            println!("cargo:rustc-env=RUSTFLAGS=-C link-arg=-Tsdram_hpc.x -C link-arg=-Tlink.x");
+        } else {
+            println!("cargo:rustc-env=RUSTFLAGS=-C link-arg=-Tmem_hpc.x -C link-arg=-Tlink.x");
+        }
+    }
+    fs::copy("sdram_hpc.x", out.join("sdram_hpc.x")).unwrap();
     fs::copy("mem_sysctrl.x", out.join("mem_sysctrl.x")).unwrap();
     println!("cargo:rustc-link-search={}", out.display());
 }
--- examples/headsail-bsp/src/lib.rs
+++ examples/headsail-bsp/src/lib.rs
@@ -11,6 +11,8 @@ pub use riscv;
 pub use riscv_rt as rt;
 pub use ufmt;
 
+#[cfg(feature = "alloc")]
+pub mod alloc;
 #[cfg(feature = "hpc")]
 mod hpc;
 mod mmap;
@@ -58,6 +60,11 @@ fn setup_interrupt_vector() {
         let bits = _trap_vector as usize;
         mtvec::write(bits, mtvec::TrapMode::Vectored);
     }
+}
+
+#[cfg(feature = "alloc")]
+pub fn init_alloc() {
+    unsafe { alloc::init_heap() };
 }
 
 // The vector table
