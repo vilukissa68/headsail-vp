4888b0740bd3a09b1df39677bfe528573c066968
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-05-06 15:51:14 +0300

    Formatting

--- examples/hpc/dla-driver/examples/mac_benchmark.rs
+++ examples/hpc/dla-driver/examples/mac_benchmark.rs
@@ -3,38 +3,38 @@
 
 extern crate alloc;
 
-use headsail_bsp::{rt::entry, sprint, sprintln, init_alloc};
 use dla_driver::*;
+use headsail_bsp::{init_alloc, rt::entry, sprint, sprintln};
 use panic_halt as _;
 
-use rand::{Rng, SeedableRng};
 use rand::rngs::SmallRng;
 use rand::RngCore;
+use rand::{Rng, SeedableRng};
 
 use alloc::vec::*;
 
 macro_rules! conv2d_out_parameters_height {
-    (($input_h:expr, $kernel_h:expr, $padding_h:expr, $dilation_h:expr, $stride_h:expr)) => {
-        {
-            ($input_h + 2 * $padding_h - $dilation_h * ($kernel_h - 1) - 1) / $stride_h + 1
-        }
-    };
+    (($input_h:expr, $kernel_h:expr, $padding_h:expr, $dilation_h:expr, $stride_h:expr)) => {{
+        ($input_h + 2 * $padding_h - $dilation_h * ($kernel_h - 1) - 1) / $stride_h + 1
+    }};
 }
 
 macro_rules! conv2d_out_parameters_width {
-    (($input_w:expr, $kernel_w:expr, $padding_w:expr, $dilation_w:expr, $stride_w:expr)) => {
-        {
-            ($input_w + 2 * $padding_w - $dilation_w * ($kernel_w - 1) - 1) / $stride_w + 1
-        }
-    };
+    (($input_w:expr, $kernel_w:expr, $padding_w:expr, $dilation_w:expr, $stride_w:expr)) => {{
+        ($input_w + 2 * $padding_w - $dilation_w * ($kernel_w - 1) - 1) / $stride_w + 1
+    }};
 }
 
-fn conv2d_output_parameters(input: (usize, usize), kernel: (usize, usize), padding: (usize, usize),
-                            dilation: (usize, usize), stride: (usize, usize)) -> (usize, usize) {
+fn conv2d_output_parameters(
+    input: (usize, usize),
+    kernel: (usize, usize),
+    padding: (usize, usize),
+    dilation: (usize, usize),
+    stride: (usize, usize),
+) -> (usize, usize) {
     let w_out = (input.0 + 2 * padding.0 - dilation.0 * (kernel.0 - 1) - 1) / stride.0 + 1;
     let h_out = (input.1 + 2 * padding.1 - dilation.1 * (kernel.1 - 1) - 1) / stride.1 + 1;
     (w_out, h_out)
-
 }
 
 fn generate_random_array(buffer: &mut [u8], size: usize) {
@@ -47,7 +47,7 @@ fn generate_random_array(buffer: &mut [u8], size: usize) {
 fn generate_random_matrix(height: usize, width: usize, seed: u64) -> Vec<u8> {
     let mut res: Vec<u8> = Vec::new();
     let mut rng = SmallRng::seed_from_u64(seed);
-    for i in 0..(height*width) {
+    for i in 0..(height * width) {
         res.push((rng.next_u64() & 0xFF) as u8);
     }
     res
@@ -56,18 +56,25 @@ fn generate_random_matrix(height: usize, width: usize, seed: u64) -> Vec<u8> {
 fn generate_random_matrix_small(height: usize, width: usize, seed: u64) -> Vec<u8> {
     let mut res: Vec<u8> = Vec::new();
     let mut rng = SmallRng::seed_from_u64(seed);
-    for i in 0..(height*width) {
+    for i in 0..(height * width) {
         res.push((rng.next_u64() & 0x1) as u8);
     }
     res
 }
 
-fn run_random_layer(dla: &mut Dla, in_w: usize, in_h: usize, k_w: usize, k_h: usize, seed: u64) -> Vec<u8> {
+fn run_random_layer(
+    dla: &mut Dla,
+    in_w: usize,
+    in_h: usize,
+    k_w: usize,
+    k_h: usize,
+    seed: u64,
+) -> Vec<u8> {
     // Generate input and kernel
     dla.init_layer();
 
     let mut input = generate_random_matrix(in_w, in_h, seed);
-    let mut kernel = generate_random_matrix_small(k_w, k_h, seed*2);
+    let mut kernel = generate_random_matrix_small(k_w, k_h, seed * 2);
 
     dla.set_kernel_size(1, k_w, k_h);
     dla.set_input_size(1, in_w, in_h);
@@ -76,19 +83,17 @@ fn run_random_layer(dla: &mut Dla, in_w: usize, in_h: usize, k_w: usize, k_h: us
     dla.write_kernel(&mut kernel);
 
     // Calculate output size
-    let (w_out, h_out) = conv2d_output_parameters((in_w, in_h), (k_w, k_h), (0,0), (1,1), (1,1));
+    let (w_out, h_out) = conv2d_output_parameters((in_w, in_h), (k_w, k_h), (0, 0), (1, 1), (1, 1));
 
     dla.kernel_data_ready(true);
     dla.input_data_ready(true);
 
     // Print the matrix
     sprintln!("Waiting for calculation");
-    while !dla.handle_handshake() {
-    }
+    while !dla.handle_handshake() {}
     sprintln!("Calculation ready");
-    let output: Vec<u8> =  dla.read_output(w_out * h_out);
+    let output: Vec<u8> = dla.read_output(w_out * h_out);
     output
-
 }
 
 #[entry]
@@ -102,7 +107,7 @@ fn main() -> ! {
     dla.set_pp_clip(8);
 
     for x in 0..2 {
-        let res = run_random_layer(&mut dla, 8,8,2,2, x*x);
+        let res = run_random_layer(&mut dla, 8, 8, 2, 2, x * x);
         for x in res {
             sprint!("{:?} ", x);
         }
