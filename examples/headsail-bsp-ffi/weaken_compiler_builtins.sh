31eb9db30f53dc33392e59c14b49bef1bdcca62b
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-05-08 15:21:23 +0300

    FFI staticlib is generated with weak symbols

---
+++ examples/headsail-bsp-ffi/weaken_compiler_builtins.sh
@@ -0,0 +1,12 @@
+mkdir -p tmp/
+cd tmp
+ARCH=riscv64-unknown-elf
+FILE=$(find .. -name libheadsail_bsp_ffi.a)
+$ARCH-ar x $FILE
+for f in compiler_builtins-*;
+do
+$ARCH-objcopy --weaken $f
+done
+$ARCH-ar cr $FILE *
+cd ..
+rm -r tmp
