d61d8ba1c64bbda2a8259e6cebc1324b7c89cd32
Andreas Stergiopoulos <32578023+andstepan@users.noreply.github.com>
2024-05-10 14:52:52 +0300

    Adds PULP APB Timer and implements single timer driver module (#21)
    * Adds PULP APB Timer and encapsulate the VP and ASIC drivers into a single module

--- examples/headsail-bsp/src/lib.rs
+++ examples/headsail-bsp/src/lib.rs
@@ -3,7 +3,15 @@
 
 pub mod sprintln;
 pub mod uart;
-pub mod timer_unit;
+pub mod timer {
+    #[cfg(not(feature = "vp"))]
+    pub use crate::apb_timer::*;
+    #[cfg(feature = "vp")]
+    pub use crate::timer_unit::*;
+}
+
+mod apb_timer;
+mod timer_unit;
 
 #[cfg(feature = "hpc")]
 pub use hpc::*;
