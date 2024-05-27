b7f35b4dbecac6b02f896f81d47230527723fa71
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-05-27 14:11:41 +0300

    Adds tiny examples for validation

--- examples/hpc/dla-driver/examples/test_data/mod.rs
+++ examples/hpc/dla-driver/examples/test_data/mod.rs
@@ -1,6 +1,6 @@
+pub mod conv_10x15x3_3x3_din;
+pub mod conv_10x15x3_3x3_dout;
+pub mod conv_10x15x3_3x3_wgt;
 pub mod conv_16x16x16_3x3_din;
-pub mod conv_16x16x16_3x3_din_by_column;
 pub mod conv_16x16x16_3x3_dout;
-pub mod conv_16x16x16_3x3_dout_by_column;
 pub mod conv_16x16x16_3x3_wgt;
-pub mod conv_16x16x16_3x3_wgt_by_column;
