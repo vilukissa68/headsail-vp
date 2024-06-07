09956259e92a2c174ae6966802cdf3b6017aa92a
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-06-07 15:59:06 +0300

    High level examples uses conv2d layer

--- examples/hpc/dla-driver/examples/highlevel.rs
+++ examples/hpc/dla-driver/examples/highlevel.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
 extern crate alloc;
 
-use dla_driver::{LayerConfig, DLA};
+use dla_driver::{Padding, Stride};
 use headsail_bsp::{init_alloc, rt::entry, sprint, sprintln};
 use panic_halt as _;
 
@@ -20,83 +21,36 @@ use alloc::vec::Vec;
 fn main() -> ! {
     init_alloc();
 
-    let din: Vec<i8> = [
+    let din: Vec<i8> = vec![
         0, 0, 0, 2, 0, 0, 1, 2, 1, 2, 0, 0, 1, 2, 0, 1, 0, 0, 0, 2, 0, 0, 1, 0, 1, 2, 0, 1, 0, 1,
         0, 0, 2, 2, 1, 1, 0, 2, 1, 1, 2, 1, 2, 2, 1, 0, 0, 1, 1, 2, 0, 1, 1, 1, 0, 0, 2, 0, 1, 2,
         1, 0, 0, 1, 2, 1, 1, 1, 0, 0, 1, 1, 2, 0, 2,
-    ]
-    .to_vec();
-    let wgt: Vec<i8> = [
+    ];
+    let wgt: Vec<i8> = vec![
         -1, -1, 0, -1, 0, 0, -1, -1, 1, 0, 0, 1, 1, -1, -1, 1, -1, 0, 1, 0, -1, -1, 1, -1, -1, 0,
         -1, 1, 0, 0, -1, 0, 1, 0, -1, 1, 0, 1, -1, -1, 0, 0, 0, -1, -1, 0, -1, 1, -1, -1, -1, 0, 1,
         0,
-    ]
-    .to_vec();
+    ];
 
-    let mut din_tensor: Tensor3<i8> = Tensor3::from_data_buffer(3, 5, 5, din, Order3::CWH).unwrap();
-
-    din_tensor.set_order(Order3::WHC);
-
-    din_tensor.print_tensor();
-
-    // for x in din_tensor.to_buffer_with_order(Order::CWH) {
-    //     sprint!("{} ", x)
-    // }
-
+    let mut din_tensor: Tensor3<i8> = Tensor3::from_data_buffer(3, 5, 5, din, Order3::CHW).unwrap();
     let mut wgt_tensor: Tensor4<i8> =
         Tensor4::from_data_buffer(2, 3, 3, 3, wgt, Order4::KCHW).unwrap();
 
-    wgt_tensor.set_order(Order4::WHKC);
+    //din_tensor.set_order(Order3::HWC);
+    //wgt_tensor.set_order(Order4::HWCK);
 
-    wgt_tensor.print_tensor();
-    let output_size = calculate_conv2d_out_param_dim((5, 5), (3, 3), (0, 0), (1, 1), (1, 1));
-
-    let dla = DLA::new();
-
-    // Initalize layer
-    let config = LayerConfig {
-        input_bank: Some(MemoryBank::Bank8),  // b
-        kernel_bank: Some(MemoryBank::Bank0), // a
-        output_bank: Some(MemoryBank::Bank10),
-        bias_addr: Some(0),
-        pp_enabled: false,
-        relu_enabled: false,
-        bias_enabled: false,
-        input_size: Some(InputSize {
-            channels: 3,
-            width: 5,
-            height: 5,
-        }),
-        kernel_size: Some(KernelSize {
-            s_channels: 1,
-            kernels: 2,
-            width: 3,
-            height: 3,
-        }),
-        padding: Some(PaddingConfig {
-            top: 0,
-            right: 0,
-            left: 0,
-            bottom: 0,
-            padding_value: 0,
-        }),
-        stride: Some(StrideConfig { x: 1, y: 1 }),
-        mac_clip: Some(0),
-        pp_clip: Some(8),
-        simd_mode: Some(SimdBitMode::EightBits),
+    let padding = Padding {
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        padding_value: 0,
     };
-
-    dla.init_layer(config);
-
-    dla.write_input(&mut din_tensor.to_buffer_with_order(Order3::HWC));
-    dla.write_kernel(&mut wgt_tensor.to_buffer_with_order(Order4::HWKC));
-
-    // Mark data ready to start calculations
-    dla.kernel_data_ready(true);
-    dla.input_data_ready(true);
-
-    while !dla.handle_handshake() {}
-    let output = dla.read_output_i32(output_size.0 * output_size.1 * 2);
+    let stride = Stride { x: 1, y: 1 };
+    let output = dla_driver::layers::conv2d(din_tensor, wgt_tensor, padding, stride);
+    sprint!("here");
+    output.print_tensor();
+    sprint!("here end");
 
     loop {}
 }
