6e1dee4b2759d9aa3aa1ba29afd87aee0ecf020f
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-06-11 10:54:59 +0300

    High level api enhancments

--- examples/hpc/dla-driver/src/layers.rs
+++ examples/hpc/dla-driver/src/layers.rs
@@ -278,3 +278,87 @@ fn run_layers<T: DlaOutput + Clone>(
     )
     .unwrap()
 }
+
+pub fn conv2d_relu(
+    input: Tensor3<i8>,
+    kernels: Tensor4<i8>,
+    padding: Padding,
+    stride: Stride,
+) -> Tensor3<i8> {
+    let output_size = calculate_conv2d_out_param_dim(
+        (input.width as u32, input.height as u32),
+        (kernels.width as u32, kernels.height as u32),
+        (padding.top, padding.right),
+        (stride.x, stride.y),
+        (1, 1),
+    );
+    let dla = Dla::new();
+
+    // Calculate needed space
+    let input_size = input.get_size();
+    let kernels_size = kernels.get_size();
+
+    let no_input_banks = calculate_number_of_banks_needed(input_size);
+    let no_kernel_banks = calculate_number_of_banks_needed(kernels_size);
+
+    let input_bank = MemoryBank::Bank0;
+    let kernel_bank = input_bank + no_input_banks;
+    let output_bank = kernel_bank + no_kernel_banks;
+
+    // Initalize layer
+    let config = LayerConfig {
+        input_bank: Some(input_bank),   // b
+        kernel_bank: Some(kernel_bank), // a
+        output_bank: Some(output_bank),
+        bias_addr: Some(0),
+        pp_enabled: true,
+        relu_enabled: true,
+        bias_enabled: false,
+        input_size: Some(InputSize {
+            channels: input.channels as u32,
+            width: input.width as u32,
+            height: input.height as u32,
+        }),
+        kernel_size: Some(KernelSize {
+            s_channels: 1,
+            kernels: kernels.kernels as u32,
+            width: kernels.width as u32,
+            height: kernels.height as u32,
+        }),
+        padding: Some(padding),
+        stride: Some(stride),
+        mac_clip: Some(0),
+        pp_clip: Some(8),
+        simd_mode: Some(SimdBitMode::EightBits),
+    };
+
+    dla.init_layer(config);
+
+    input.print_tensor();
+    kernels.print_tensor();
+
+    dla.write_input(&mut input.to_buffer_with_order(Order3::HWC));
+    dla.write_kernel(&mut kernels.to_buffer_with_order(Order4::HWCK));
+
+    // Mark data ready to start calculations
+    dla.kernel_data_ready(true);
+    dla.input_data_ready(true);
+
+    while !dla.handle_handshake() {}
+    let output_buffer = dla.read_output_i8(output_size.0 * output_size.1 * kernels.kernels);
+
+    sprintln!("Raw output:");
+    for x in &output_buffer {
+        sprint!("{} ", x)
+    }
+
+    let output: Tensor3<i8> = Tensor3::from_data_buffer(
+        kernels.kernels,
+        output_size.1,
+        output_size.0,
+        output_buffer,
+        Order3::WHC, // NOTE: (20240610 vaino-waltteri.granat@tuni.fi) This might not be true on ASIC
+    )
+    .unwrap();
+    output
+}
--- examples/hpc/dla-driver/src/utils.rs
+++ examples/hpc/dla-driver/src/utils.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 use crate::mmap::MEMORY_BANK_SIZE;
+use crate::Padding;
 
 pub fn calculate_conv2d_out_param_dim(
     input: (u32, u32),
@@ -17,4 +18,44 @@ pub fn calculate_conv2d_out_param_dim(
 pub fn calculate_number_of_banks_needed(bytes: usize) -> usize {
     // Take ceil
     (bytes + (MEMORY_BANK_SIZE - 1)) / MEMORY_BANK_SIZE
+}
+
+fn ceil<T>(x: T, y: T) -> T
+where
+    T: core::ops::Add<Output = T>
+        + core::ops::Sub<Output = T>
+        + core::ops::Div<Output = T>
+        + From<u8>
+        + Copy,
+{
+    (x + y - T::from(1)) / y
+}
+
+pub fn calculate_valid_output_size(
+    input: (u32, u32),
+    kernel: (u32, u32),
+    stride: (u32, u32),
+) -> (usize, usize) {
+    let output_width = ceil(input.0 - kernel.0 + 1, stride.0);
+    let output_height = ceil(input.1 - kernel.1 + 1, stride.1);
+    (output_width as usize, output_height as usize)
+}
+
+pub fn calculate_same_output_size(input: (u32, u32), stride: (u32, u32)) -> (usize, usize) {
+    let output_width = ceil(input.0, stride.0);
+    let output_height = ceil(input.1, stride.1);
+    (output_width as usize, output_height as usize)
+}
+
+fn calculate_same_padding(input: (u32, u32), kernel: (u32, u32), stride: (u32, u32)) -> Padding {
+    let padding_width = ((input.0 - 1) * stride.0 + kernel.0).saturating_sub(input.0);
+    let padding_height = ((input.1 - 1) * stride.1 + kernel.1).saturating_sub(input.1);
+
+    Padding {
+        top: padding_height / 2,
+        bottom: padding_height - padding_height / 2,
+        left: padding_width / 2,
+        right: padding_width - padding_width / 2,
+        padding_value: 0,
+    }
 }
