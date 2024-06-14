7f888fc80ebc127c0fb68380345f0402c68f810d
Vaino Granat <vaino-waltteri.granat@tuni.fi>
2024-06-14 12:16:12 +0300

    Adds ability to read from uart to heap (#42)
    Adds ability to read uart to heap. This is only available when GlobalAllocator is defined

--- examples/headsail-bsp/src/mmap.rs
+++ examples/headsail-bsp/src/mmap.rs
@@ -1,4 +1,6 @@
 pub(crate) const UART0_ADDR: usize = 0xFFF00000;
+// NOTE: (20240614 vaino-waltteri.granat@tuni.fi) This applies to renode NS16550 uart, but might not apply to headsail ASIC
+pub(crate) const UART_DATA_READY_OFFSET: usize = 5;
 pub(crate) const TIMER0_ADDR: usize = 0x5_0000;
 pub(crate) const TIMER1_ADDR: usize = 0x5_0010;
 pub(crate) const TIMER2_ADDR: usize = 0x5_0020;
--- examples/headsail-bsp/src/uart.rs
+++ examples/headsail-bsp/src/uart.rs
@@ -1,4 +1,9 @@
-use crate::{mmap::UART0_ADDR, write_u8};
+use crate::{mmap::UART0_ADDR, mmap::UART_DATA_READY_OFFSET, read_u8, write_u8};
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+#[cfg(feature = "alloc")]
+use alloc::vec::Vec;
 
 #[inline]
 pub fn uart_write(s: &str) {
@@ -11,4 +16,22 @@ pub fn uart_write(s: &str) {
 pub fn putc(c: u8) {
     // Safety: we don't know if u8 writes work for all target architectures
     unsafe { write_u8(UART0_ADDR, c) };
+}
+
+#[inline]
+pub fn getc() -> u8 {
+    // Wait for data to become ready
+    while unsafe { read_u8(UART0_ADDR + UART_DATA_READY_OFFSET) } & 1 == 0 {}
+
+    // SAFETY: UART0_ADDR is 4-byte aligned
+    unsafe { read_u8(UART0_ADDR) }
+}
+
+#[cfg(feature = "alloc")]
+pub fn uart_read_to_heap(bytes: usize) -> Vec<u8> {
+    let mut result = Vec::with_capacity(bytes);
+    for _ in 0..bytes {
+        result.push(getc())
+    }
+    result
 }
