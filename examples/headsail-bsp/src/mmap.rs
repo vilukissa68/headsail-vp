de2a07a013ab8a45907d49e00a6f6fd660b7bc4a
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-06-14 11:34:08 +0300

    Fixes mmap

--- examples/headsail-bsp/src/mmap.rs
+++ examples/headsail-bsp/src/mmap.rs
@@ -1,6 +1,3 @@
-#[cfg(feature = "hpc")]
-pub(crate) const UART0_ADDR: usize = 0xFFF00000;
-#[cfg(not(feature = "hpc"))]
 pub(crate) const UART0_ADDR: usize = 0xFFF00000;
 // NOTE: (20240614 vaino-waltteri.granat@tuni.fi) This applies to renode NS16550 uart, but might not apply to headsail ASIC
 pub(crate) const UART_DATA_READY_OFFSET: usize = 5;
