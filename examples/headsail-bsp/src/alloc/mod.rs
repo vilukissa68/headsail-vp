558cdb78250078d9fc86a772f89ebeeb3f256688
Vaino Granat <vaino-waltteri.granat@tuni.fi>
2024-06-19 14:26:08 +0300

    Add support for HPC external bit in DLA (#44)
    * Adds support for HPC external bit in DLA
    * Better document external address normalization
    * Adds hpc feature to dla

--- examples/headsail-bsp/src/alloc/mod.rs
+++ examples/headsail-bsp/src/alloc/mod.rs
@@ -1,5 +1,8 @@
 use good_memory_allocator::SpinLockedAllocator;
 
+#[cfg(feature = "hpc")]
+const HEAP_START: usize = 0x130000000;
+#[cfg(not(feature = "hpc"))]
 const HEAP_START: usize = 0x30000000;
 const HEAP_SIZE: usize = 0x10000000;
 
