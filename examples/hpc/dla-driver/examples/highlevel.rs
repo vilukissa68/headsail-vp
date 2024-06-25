af7b9d715e64fed15cff3f198721d48cd2f26cb8
Väinö Granat <vaino.granat1@gmail.com>
2024-06-25 17:24:28 +0300

    Makes sense of the tensor ordering

--- examples/hpc/dla-driver/examples/highlevel.rs
+++ examples/hpc/dla-driver/examples/highlevel.rs
@@ -48,39 +48,45 @@ fn main() -> ! {
     };
     let stride = Stride { x: 1, y: 1 };
     let mut output = dla_driver::layers::conv2d(din_tensor, wgt_tensor, padding, stride);
-    output.transmute(Order3::CWH);
+    //output.transmute(Order3::CWH);
     output.print_tensor();
     dout_tensor.print_tensor();
 
     // Larger
-    // let mut din_large: Vec<i8> = conv_16x16x16_3x3_din::DATA
-    //     .iter()
-    //     .map(|&x| x as i8)
-    //     .collect();
-    // let mut dout_large: Vec<i32> = conv_16x16x16_3x3_dout::DATA
-    //     .iter()
-    //     .map(|&x| x as i32)
-    //     .collect();
-    // let mut wgt_large: Vec<i8> = conv_16x16x16_3x3_wgt::DATA
-    //     .iter()
-    //     .map(|&x| x as i8)
-    //     .collect();
-    // let mut din_large_tensor: Tensor3<i8> =
-    //     Tensor3::from_data_buffer(16, 16, 16, din_large, Order3::CHW).unwrap();
-    // let mut wgt_large_tensor: Tensor4<i8> =
-    //     Tensor4::from_data_buffer(16, 16, 3, 3, wgt_large, Order4::KCHW).unwrap();
-    // let padding = Padding {
-    //     top: 0,
-    //     left: 0,
-    //     right: 0,
-    //     bottom: 0,
-    //     padding_value: 0,
-    // };
-    // let stride = Stride { x: 1, y: 1 };
-    // let mut output =
-    //     dla_driver::layers::conv2d(din_large_tensor, wgt_large_tensor, padding, stride);
-    // output.transmute(Order3::CWH);
-    // output.print_tensor();
+    let mut din_large: Vec<i8> = conv_16x16x16_3x3_din::DATA
+        .iter()
+        .map(|&x| x as i8)
+        .collect();
+    let mut dout_large: Vec<i32> = conv_16x16x16_3x3_dout::DATA
+        .iter()
+        .map(|&x| x as i32)
+        .collect();
+    let mut wgt_large: Vec<i8> = conv_16x16x16_3x3_wgt::DATA
+        .iter()
+        .map(|&x| x as i8)
+        .collect();
+    let mut din_large_tensor: Tensor3<i8> =
+        Tensor3::from_data_buffer(16, 16, 16, din_large, Order3::CHW).unwrap();
+    let mut wgt_large_tensor: Tensor4<i8> =
+        Tensor4::from_data_buffer(16, 16, 3, 3, wgt_large, Order4::KCHW).unwrap();
+    let padding = Padding {
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        padding_value: 0,
+    };
+    let stride = Stride { x: 1, y: 1 };
+    let mut output =
+        dla_driver::layers::conv2d(din_large_tensor, wgt_large_tensor, padding, stride);
+    output.transmute(Order3::CWH);
+    output.print_tensor();
+
+    let test: Vec<i8> = vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+    let mut test_tensor: Tensor3<i8> = Tensor3::from_data_buffer(3,3,3, test, Order3::HCW).unwrap();
+    test_tensor.transmute(Order3::CHW);
+    test_tensor.print_tensor();
+
 
     loop {}
 }
