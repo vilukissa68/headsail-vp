6fc118fdc0beb8dc46fceee02e70dbbace219f8c
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-07-03 13:56:45 +0300

    Adds example for building with newlib

---
+++ examples/hpc-c/hello-newlib/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include "boot.h"
+
+//extern int _write();
+int main()
+{
+    printf("Hello world\n");
+    printf("VBVA");
+    int x = 39;
+    for(int i = 0; i < x ;++i){
+        printf("A");
+    }
+    for(;;)
+    return 0;
+}
