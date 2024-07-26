0a728b013e9f6ac6e5b5268d0d992eb42770cfc6
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-07-26 10:45:43 +0300

    Adds padding class

--- vp/devel/python_peripherals/DLA.py
+++ vp/devel/python_peripherals/DLA.py
@@ -454,6 +454,12 @@ def rounding(value):
     """
     return round(value)
 
+class Padding:
+    def __init__(self, left, right, top, bottom):
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
 
 # DLA
 class MemoryBank:
