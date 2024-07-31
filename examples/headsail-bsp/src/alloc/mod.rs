b84461efad9d4bf9eaffee7dd9c7d2fb752e7905
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-07-31 10:06:11 +0300

    Removes change to sysctrl sdram allocation

--- examples/headsail-bsp/src/alloc/mod.rs
+++ examples/headsail-bsp/src/alloc/mod.rs
@@ -3,7 +3,7 @@ use good_memory_allocator::SpinLockedAllocator;
 #[cfg(feature = "hpc")]
 const HEAP_START: usize = 0x130000000;
 #[cfg(not(feature = "hpc"))]
-const HEAP_START: usize = 0x130000000;
+const HEAP_START: usize = 0x30000000;
 const HEAP_SIZE: usize = 0x10000000;
 
 #[global_allocator]
