7495c71f2388c4335bb4299d0ac7c279aacbfbe5
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-08-06 11:59:05 +0300

    Data sending works for all datasets

--- examples/hpc-c/tvm-hpc/tiny_perf_benchmark.py
+++ examples/hpc-c/tvm-hpc/tiny_perf_benchmark.py
@@ -11,8 +11,12 @@ import sys
 import pylab
 import re
 import serial
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import tensorflow as tf
 
 
+
+UART = '/tmp/uart0'
 ROOT_PATH = Path(__file__).parents[0]
 DATA_DIR = ROOT_PATH / "dev_data"
 KWS_DATA_DIR = DATA_DIR / "kws01"
@@ -22,60 +26,100 @@ VWW_PERSON_DATA_DIR = VWW_DATA_DIR / "person"
 IC_DATA_DIR = DATA_DIR / "cifar-10-batches-py"
 AD_DATA_DIR = DATA_DIR / "ToyCar" / "test"
 
+def accuracy_report(gt, prediction):
+    print("Accuracy: {:.3f}".format(accuracy_score(gt, prediction)))
+    print("Confusion matrix:\n{}".format(confusion_matrix(gt, prediction)))
+    print(classification_report(gt, prediction))
+
+def send_stimulus(data):
+    ser = serial.Serial(UART, 9600)
+    ser.write(data)
+    ser.close()
+
+def wait_for_result():
+    return 1
+    ser = serial.Serial(UART,  9600)
+    output = ser.readline()
+    ser.close()
+    return output
+
 def read_kws_file(path):
     with open(path, mode="rb") as file:
         content = file.read()
     return content
 
 def run_kws():
-    # Find all files in the KWS data directory
-    #items = os.listdir(KWS_DATA_DIR)
-    #files = [item for item in items if os.path.isfile(os.path.join(KWS_DATA_DIR, item))]
     df = pd.read_csv(KWS_DATA_DIR / "y_labels.csv", names=["filename", "no_classes", "class"])
-    print(df.to_string())
+    print("Input shape: ( ,{})".format(len(read_kws_file(KWS_DATA_DIR / df["filename"][0]))))
+
+    predictions = []
     for (i, filename) in enumerate(df["filename"]):
-        label = df["class"][i]
         data = read_kws_file(KWS_DATA_DIR / filename)
-        for x in data:
-            print("{:x}".format(x))
+        send_stimulus(data)
+
+        predictions.append(wait_for_result())
+
+    accuracy_report(df["class"], predictions)
 
 def read_vww_file(path):
-    image = Image.open(path)
-    image.show()
-    byte_array = io.BytesIO()
-    image.save(byte_array, format="PNG")
-    return byte_array.getvalue()
+    #Image loading and preprocessing
+    image = tf.io.read_file(str(path))
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [96,96])
+    image = tf.cast(image, tf.uint8)
+    byte_array = image.numpy()
+    return byte_array
 
 def run_vww():
     items = os.listdir(VWW_NON_PERSON_DATA_DIR)
-    non_persons = [item for item in items if os.path.isfile(os.path.join(VWW_NON_PERSON_DATA_DIR, item))]
+    non_persons = [item for item in items if os.path.isfile(os.path.join(VWW_NON_PERSON_DATA_DIR, item)) and item.startswith("COCO_val")]
 
     items = os.listdir(VWW_PERSON_DATA_DIR)
-    persons = [item for item in items if os.path.isfile(os.path.join(VWW_PERSON_DATA_DIR, item))]
+    persons = [item for item in items if os.path.isfile(os.path.join(VWW_PERSON_DATA_DIR, item)) and item.startswith("COCO_val")]
 
-    print(non_persons)
-    print(persons)
-    print(read_vww_file(VWW_PERSON_DATA_DIR / persons[0]))
+    print("Number of non_persons", len(non_persons))
+    print("Number of persons", len(persons))
+    print("Input shape: ", np.shape(read_vww_file(VWW_NON_PERSON_DATA_DIR / non_persons[0])))
 
+    # Generate ground truth array
+    gt_non = [0 for _ in non_persons]
+    gt_yes = [1 for _ in persons]
+    gt = gt_non + gt_yes
+
+    predictions = []
+    for x in non_persons:
+        data = read_vww_file(VWW_NON_PERSON_DATA_DIR / x)
+
+        send_stimulus(data.tobytes())
+        predictions.append(wait_for_result())
+
+    for x in persons:
+        data = read_vww_file(VWW_PERSON_DATA_DIR / x)
+        send_stimulus(data.tobyte())
+        predictions.append(wait_for_result())
+
+    accuracy_report(gt, predictions)
 
 def run_ic():
     import pickle
     with open(IC_DATA_DIR / "test_batch", "rb") as file:
         data = pickle.load(file, encoding='bytes')
-    for (i, image) in enumerate(data[b'data']):
-        label = data[b'labels'][i]
+    print("Input shape: {}".format(np.shape(data[b'data'][0])))
+
+    predictions = []
+    for image in data[b'data']:
         send_stimulus(image.tobytes())
 
-def send_stimulus(data):
-    ser = serial.Serial('/tmp/uart0', 9600)
-    #print(data)
-    ser.write(b'0xff')
-    ser.write(data)
-    ser.close()
+        # Wait for inference result
+        predictions.append(wait_for_result())
+        pass
+
+    # Evaluate predictions
+    accuracy_report(data[b'labels'], predictions)
 
 def main():
     #run_kws()
-    #run_vww()
-    run_ic()
+    run_vww()
+    #run_ic()
 
 main()
