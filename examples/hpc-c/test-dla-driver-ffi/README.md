6610b69e979cefd9f6801387ff959ad90547e552
Vaino Granat <vaino-waltteri.granat@tuni.fi>
2024-08-19 09:42:08 +0300

    Add test case for DLA FFI (#58)
    #57 added C interface for DLA driver. This commit introduces a simple test case for the interface.

---
+++ examples/hpc-c/test-dla-driver-ffi/README.md
@@ -0,0 +1,7 @@
+# DLA-FFI test
+
+Bare metal C program that links with Headsail DLA (Rust).
+
+## Compile & run
+
+Sa [hello-hpc](../hello-hpc/README.md).
---
+++ examples/hpc-c/test-dla-driver-ffi/main.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "dla_driver.h"
+
+int main()
+{
+    printf("DLA FFI Test start!\n");
+    dla_init();
+    int8_t A[9] = {1,2,3,4,5,6,7,8,9};
+    int8_t B[4] = {1,2,3,4};
+    int16_t bias[1] = {20};
+    char * input_order = "CHW";
+    char * kernel_order = "KCHW";
+    int8_t* C = malloc(4);
+    dla_conv2d_relu(A, 1, 3, 3, input_order, B, 1, 1, 2, 2, kernel_order, 0, 0, 0, 0, 0, 1, 1, 0 ,0, C);
+    for(int i = 0; i < 4; ++i) {
+        printf("%d ", C[i]);
+    }
+    free(C);
+    printf("Done!\n");
+    for(;;){}
+}
