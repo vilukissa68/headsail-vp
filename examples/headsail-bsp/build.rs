3c351e5b6de1608e20d0e39ccdb1543b27369746
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-09-25 11:48:50 +0300

    Add linker script for sysctrl sdram

--- examples/headsail-bsp/build.rs
+++ examples/headsail-bsp/build.rs
@@ -9,14 +9,16 @@ fn main() {
     // Put link script in our output directory and ensure it's on the linker search path
     let out = &path::PathBuf::from(env::var_os("OUT_DIR").unwrap());
     fs::copy("mem_hpc.x", out.join("mem_hpc.x")).unwrap();
-    if cfg!(feature = "hpc") {
-        if cfg!(feature = "sdram") {
+
+    if cfg!(feature = "sdram") {
+        if cfg!(feature = "hpc") {
             println!("cargo:rustc-env=RUSTFLAGS=-C link-arg=-Tsdram_hpc.x -C link-arg=-Tlink.x -C linker=riscv64-unknown-elf-ld");
         } else {
-            println!("cargo:rustc-env=RUSTFLAGS=-C link-arg=-Tmem_hpc.x -C link-arg=-Tlink.x");
+            println!("cargo:rustc-env=RUSTFLAGS=-C link-arg=-Tsdram_sysctrl.x -C link-arg=-Tlink.x -C linker=riscv32-unknown-elf-ld");
         }
     }
     fs::copy("sdram_hpc.x", out.join("sdram_hpc.x")).unwrap();
+    fs::copy("sdram_sysctrl.x", out.join("sdram_sysctrl.x")).unwrap();
     fs::copy("mem_sysctrl.x", out.join("mem_sysctrl.x")).unwrap();
     println!("cargo:rustc-link-search={}", out.display());
 }
